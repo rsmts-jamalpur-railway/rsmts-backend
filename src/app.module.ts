fa643d752e7993ff2bae4e8f0d04303cd126bd56
Md Farhan <farhannaiyyar04@gmail.com>
2026-09-08 08:37:48 +0530

    fix(app): change NODE_ENV check for pino-pretty to avoid 500 on railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -45,10 +45,10 @@ import { SettingsModule } from './settings/settings.module';
     LoggerModule.forRoot({
       pinoHttp: {
         transport:
-          process.env.NODE_ENV !== 'production'
+          process.env.NODE_ENV === 'development'
             ? { target: 'pino-pretty' }
             : undefined,
-        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
       },
     }),
 
